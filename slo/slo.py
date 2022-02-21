d70884ab477d6fdf447c2ef073ab2e20dd301bd2
JakWai01 <jakob.waibel@web.de>
2022-02-21 15:49:02 +0100

    feat: Add utils to check for valid urls

--- slo/slo.py
+++ slo/slo.py
@@ -1,11 +1,11 @@
 import click
-from lib.request import request
+from lib.enumerate import enumerate
 
 @click.command()
 @click.option('-w', '--wordlist', help='A wordlist for directory enumeration')
 @click.option('-u', '--url', help='The URL to be enumerated')
 def cli(wordlist, url):
-   request("hans", "werner")
+    enumerate(wordlist, url)
 
 if __name__ == '__main__':
    cli()
